c338f5aecf4004087a0c2417609c00b9d8abaf92
obadx <abdullahamlyossef@gmail.com>
2025-02-02 18:43:05 +0200

    add: initial working version of gnerate_ds

--- generate_datasets.py
+++ generate_datasets.py
@@ -2,5 +2,9 @@ from recitations_segmenter.train.process_data import to_huggingface_dataset
 
 if __name__ == '__main__':
     recitations_file = './recitations.yml'
-    ds = to_huggingface_dataset(recitations_file, base_dir='./data')
+    ds = to_huggingface_dataset(
+        recitations_file, base_dir='./data', num_proc=8, limit=10)
     print(ds)
+
+    for k, v in ds['recitation_0'][0].items():
+        print(f'{k}:\n{v}\n\n')
--- src/recitations_segmenter/train/process_data.py
+++ src/recitations_segmenter/train/process_data.py
@@ -1,10 +1,18 @@
 from pathlib import Path
 import yaml
-from datasets import Dataset, DatasetDict, load_dataset, Audio
+from datasets import Dataset, DatasetDict, load_dataset, Audio, Array2D
 from dataclasses import dataclass
 import os
+import torch
+import numpy as np
 
-from ..utils import download_file_fast, get_audiofiles, save_jsonl, SURA_TO_AYA_COUNT
+from ..utils import (
+    download_file_fast,
+    get_audiofiles,
+    save_jsonl,
+    SURA_TO_AYA_COUNT,
+)
+from .vad_utils import quran_split_by_silence
 
 
 @dataclass
@@ -14,6 +22,10 @@ class Recitation:
     url: str
     dataset: Dataset = None
     download_path: Path = None
+    window_size_samples: int = 1536
+    threshold: float = 0.3
+    min_silence_duration_ms: float = 300
+    pad_duration_ms: float = 30
 
 
 def valid_aya_format(p: Path) -> bool:
@@ -69,7 +81,7 @@ def generate_ds(recitation: Recitation, ds_path: Path) -> Dataset:
             'file_name': str(p.relative_to(ds_path)),
             'aya_name': p.name.split('.')[0],
             'reciter_name': recitation.reciter_name,
-            'reciter_id': recitation.id,
+            'recitation_id': recitation.id,
             'url': recitation.url,
         })
     save_jsonl(metadata, ds_path / 'metadata.jsonl')
@@ -77,17 +89,54 @@ def generate_ds(recitation: Recitation, ds_path: Path) -> Dataset:
     return ds['train']
 
 
-def to_huggingface_dataset(recitations_file: str | Path, base_dir='data') -> DatasetDict:
+def extarct_speech_intervals(
+    dataset: Dataset,
+    recitiation: Recitation,
+    num_proc=8,
+) -> Dataset:
+
+    def intervals_map(item):
+        out = quran_split_by_silence(
+            torch.tensor(item['audio']['array'], dtype=torch.float32),
+            window_size_samples=recitiation.window_size_samples,
+            min_silence_duration_ms=recitiation.min_silence_duration_ms,
+            pad_duration_ms=recitiation.pad_duration_ms,
+            threshold=recitiation.threshold,
+            sample_rate=16000,
+            device='cuda',
+        )
+        is_complete = out.clean_intervals.view(-1,)[-1] != float('inf')
+        return {
+            'speech_intervals': out.clean_intervals.cpu().numpy(),
+            'is_interval_complete': is_complete,
+        }
+
+    ds = dataset.map(intervals_map, num_proc=num_proc)
+    return ds
+
+
+def to_huggingface_dataset(
+    recitations_file: str | Path,
+    base_dir='data',
+    num_proc=8,
+    limit: int = None,
+) -> DatasetDict:
     """Converting Audio files to hugginface audio dataset
+
+    Args:
+        num_proc (int): number of parallel tasks to process the dataset
+        limit (int): for testing only take out number of `limit` samples
     """
 
     base_dir = Path(base_dir)
     # Load the YAML file
     recitations = []
+    idx_to_recitation = {}
     with open(recitations_file, 'r', encoding='utf-8') as file:
         data = yaml.safe_load(file)['recitations']
     for rec in data:
         recitations.append(Recitation(**rec))
+        idx_to_recitation[rec['id']] = Recitation(**rec)
 
     # Downloading and extracting all rectitations
     for idx in range(len(recitations)):
@@ -104,10 +153,24 @@ def to_huggingface_dataset(recitations_file: str | Path, base_dir='data') -> Dat
     # concatenated dataset as datasetdict with key is the  reciter_id
     dataset_dict = DatasetDict()
     for rec in recitations:
-        dataset_dict[str(rec.id)] = rec.dataset
+        dataset_dict[f'recitation_{rec.id}'] = rec.dataset
 
     # cast dataset to sampling_rate 16000
     dataset_dict = dataset_dict.cast_column(
         'audio', Audio(sampling_rate=16000))
+
+    print(dataset_dict)
+
+    # extract speech intervals
+    for rec_id in dataset_dict:
+        id = int(rec_id.split('_')[-1])
+        ds = dataset_dict[rec_id]
+        if limit:
+            ds = ds.select(range(limit))
+
+        dataset_dict[rec_id] = extarct_speech_intervals(
+            ds, idx_to_recitation[id])
+    dataset_dict = dataset_dict.cast_column(
+        'speech_intervals', Array2D(shape=(None, 2), dtype="float32"))
 
     return dataset_dict
