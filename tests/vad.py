ce9018295ce8d589ceb8fca4a6485b5147447a46
obadx <abdullahamlyossef@gmail.com>
2025-02-02 18:41:54 +0200

    add: testing form hf dataset

--- tests/vad.py
+++ tests/vad.py
@@ -1,9 +1,16 @@
+from datasets import load_dataset
+import torch
 from recitations_segmenter.train .vad_utils import read_audio, quran_split_by_silence
 
 
 if __name__ == '__main__':
     file_path = '/home/abdullah/Downloads/001.adts'
     wav = read_audio(file_path)
+
+    ds = load_dataset('abdullahaml/test')['train']
+    wav = ds[0]['audio']['array']
+    wav = torch.tensor(wav, dtype=torch.float32)
+
     out = quran_split_by_silence(
         wav,
         sample_rate=16000,
@@ -11,7 +18,7 @@ if __name__ == '__main__':
         min_silence_duration_ms=400,
         pad_duration_ms=30,
         threshold=0.3,
-        device='cuda',
+        device='cpu',
     )
     print(out.clean_intervals.shape)
     print(out.clean_intervals)
