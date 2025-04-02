707bafbbf310f4c9c8cec445cb9f95d4b1d6bdbd
obadx <abdullahamlyossef@gmail.com>
2025-04-02 22:55:58 +0200

    force to load conda libraries

--- hpc_scripts/apply-augments.sh
+++ hpc_scripts/apply-augments.sh
@@ -5,6 +5,9 @@ source /cluster/users/shams035u1/data/miniconda3/bin/activate
 conda activate segment12
 cd ..
 
+# Issue in the loading libraries of scipy (forcing to load conda g++ libraries
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
+
 BASE_PATH=/cluster/users/shams035u1/data/segmentation-datasets
 
 python apply_augments.py \
