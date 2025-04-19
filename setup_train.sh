73a14913f460baf10e66102c8467c15a2171383b
obadx <abdullahamlyossef@gmail.com>
2025-04-19 23:19:20 +0200

    using accelerate config

--- setup_train.sh
+++ setup_train.sh
@@ -3,3 +3,4 @@ cd recitations-segmenter
 git checkout train
 pip install -r train_requirements.txt
 python -c "from datasets import load_dataset; ds = load_dataset('obadx/recitation-segmentation-augmented', num_proc=32)"
+accelerate config
