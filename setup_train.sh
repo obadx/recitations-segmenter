7a98865d1f2bc757cbb559289fa4f7fcf8effa84
obadx <abdullahamlyossef@gmail.com>
2025-04-19 23:14:09 +0200

    using train branch

--- setup_train.sh
+++ setup_train.sh
@@ -1,3 +1,5 @@
 git clone git@github.com:obadx/recitations-segmenter.git
+cd recitations-segmenter
+git checkout train
 pip install -r train_requirements.txt
 python -c "from datasets import load_dataset; ds = load_dataset('obadx/recitation-segmentation-augmented', num_proc=32)"
